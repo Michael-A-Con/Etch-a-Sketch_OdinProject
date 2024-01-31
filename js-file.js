888f6efaac6535f8e4ad988f3751f25f84b2e6fe
Michael-A-Con <michaelandrescon@gmail.com>
2024-01-31 15:56:04 -0600

    finished project

--- js-file.js
+++ js-file.js
@@ -1,9 +1,36 @@
-const container = document.getElementById('grid-container');
-alert(hello);
-for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
-        const gridItem = document.createElement('div');
-        gridItem.classList.add('grid-item');
-        container.appendChild(gridItem);
+    // Create grid squares using JavaScript    
+    function createTable(dimension) {
+        let container = document.querySelector('#container');
+
+        for (rows = 0; rows < dimension; rows++) {
+            let divRows = document.createElement('div');
+            divRows.className = 'columns';
+            for (column = 0; column < dimension; column++) {
+                let divColumns = document.createElement('div');
+                divColumns.className = 'squares'
+                divRows.append(divColumns);
+            }
+            container.append(divRows);
+        }
+
+        let squares = document.querySelectorAll('.squares');
+        for (let i = 0; i < squares.length; i++) {
+            squares[i].addEventListener('mouseover', function (e) {
+                squares[i].className = 'used';
+            })
+        }
     }
-}
+
+    function deleteTable() {
+        let container = document.querySelector('#container');
+        while (container.firstChild) {
+            container.removeChild(container.lastChild);
+        }
+    }
+
+    document.querySelector('button').addEventListener('click', function (e) {
+        deleteTable();
+        createTable(parseInt(prompt("Dimensions?")));
+    })
+
+    createTable(16);
